46da7633c7eb63ccdd5869290189465955eec0d5
Imron R <ymronz@gmail.com>
2023-03-25 08:56:55 +0700

    menambahka controller part 1

--- app/core/App.php
+++ app/core/App.php
@@ -1,20 +1,39 @@
 <?php
 
 class App {
-	protected $controller = 'Home';
+	protected $controller = 'home';
 	protected $method = 'index';
 	protected $params = [];
 
 	public function __construct()	{
 		$url = $this->parseURL();
 		//var_dump('../app/controllers/' . $url[0] . '.php');
-
+		//$url[0] = 'home';
+		//controller
 		if (file_exists('../app/controllers/' . $url[0] . '.php')) {
-			// code...
 			$this->controller = $url[0];
 			unset($url[0]);
-			var_dump($url);
 		}
+		require_once '../app/controllers/' . $this->controller . '.php';
+		$this->controller = new $this->controller;
+
+		//method
+		if(isset($url[1])) {
+			if (method_exists($this->controller, $url[1])) {
+				// code...
+				$this->method = $url[1];
+				unset($url[1]);
+			}
+		}
+
+		//params
+		if (!empty($url)) {
+			// code...
+			$this->params = array_values($url);
+		}
+
+		//jalankan controller & method, serta kirimkan params jika ada
+		call_user_func_array([$this->controller, $this->method], $this->params);
 	}
 
 	public function parseURL()
