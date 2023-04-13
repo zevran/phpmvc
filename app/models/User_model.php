9a271011e7653ec5f4b220c4e63a19b937bfc6c4
Imron R <ymronz@gmail.com>
2023-04-13 11:53:05 +0700

    Menambahkan Models

---
+++ app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php
+
+class User_model {
+	private $nama = 'Olakan';
+
+	public function getUser()
+	{
+		return $this->nama;
+	}
+}
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -2,7 +2,7 @@
 	<div class="my-4">
         <div class="h-100 p-5 text-bg-dark rounded-3">
           <h2>Ini adalah halaman home</h2>
-          <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
+          <p>Halo ! Nama saya <?= $data['nama']; ?></p>
           <hr class="my-4">
           <button class="btn btn-outline-light" type="button">Example button</button>
         </div>
