4b2657677b288fea5c2e56861416486857c013f4
Imron R <ymronz@gmail.com>
2023-04-13 09:41:44 +0700

    Menambahkan file asset Bootstrap V5.3

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
+<div class="container my-4">
 	<h1>Halaman About Index</h1>
+	<img src="<?= BASEURL ?>/img/BDD Tiket.png" alt="Nama Profile" width="250" class="rounded-circle shadow">
 	<p>Halo, Nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan'];?>. </p>
+</div>
